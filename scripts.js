32b25a737bf5eddc447ad02e53ec0c479a908135
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2020-11-14 03:20:55 +0100

    fetch dom elements

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let domElements = {
+    heading: document.querySelector('h2'),
+    video: document.querySelector('video'),
+    fact: document.querySelector('.fact-text'),
+    name: document.querySelector('.name-text'),
+    atomicNo: document.querySelector('.am-text'),
+    massNo: document.querySelector('.mn-text'),
+    year: document.querySelector('.year-text'),
+    nature: document.querySelector('.nature-text'),
+    eps: document.querySelector('.eps-text'),
+    ec: document.querySelector('.ec-text')
+}
